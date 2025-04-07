4c6cc2f25c4f046a1c8fb8ddb2493b2364f94bc2
mdmarufulislamkhan <mdmarufulislamkhan1@gmail.com>
2025-04-07 16:13:16 +0600

    2nd commit

--- view/user_action.php
+++ view/user_action.php
@@ -1,19 +1,47 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Registration Successful</title>
+    <link rel="stylesheet" type="text/css" href="../css/khanStyle.css">
+    <style>
+        body {
+            background: linear-gradient(to right, #0f2027, #203a43, #2c5364);
+            color: white;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            text-align: center;
+            font-family: 'Poppins', sans-serif;
+        }
+        .success-box {
+            background: rgba(255, 255, 255, 0.1);
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.3);
+            max-width: 500px;
+        }
+        h1 {
+            font-size: 2.5rem;
+            color: #f5f5f5;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+        .thank-you {
+            font-size: 1.2rem;
+            margin-top: 15px;
+            background: rgba(255, 165, 0, 0.8);
+            padding: 10px 15px;
+            border-radius: 5px;
+            display: inline-block;
+        }
+        
+    </style>
+</head>
 <body>
-    <br>
-    <br>
-    <br>
-    <br>
-    <link rel="stylesheet" type="text/css" href="../css/khan-style.css">
-    <h1> <p id="id">Your User Registration is complete! </p> </h1>
-      
-    <table align="center">
-         <div align="center">
-             Thank You!
-         </div>
- 
-         
-     </table>
+    <div class="success-box">
+        <h1>Your User Registration is Complete!</h1>
+        <p class="thank-you">Thank You!</p>
+    </div>
 </body>
 </html>
